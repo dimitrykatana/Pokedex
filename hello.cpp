91c40b28bcb6558da59046401db2f023b4e75d82
dimitrykatana <dimitrykatana@gmail.com>
2022-09-28 00:05:32 +0200

    first test

--- hello.cpp
+++ hello.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
 
     // cout stands for Character OUTput.
     // the endl passes a condition to the application that the 
-    //curser should move to the next line and so it prints  what is behind
+    //curser should move to the next line and so it prints what is behind
     cout << "Welcome to kano" << endl;
     // I let the std here to see how it works
     std::cin.get();
